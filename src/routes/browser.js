cdea849d28e05d40573f82f75d5f908457594df5
thilakeshwar2024 <thilakeshwar2024@outlook.com>
2024-05-18 08:41:25 +0530

    Update browser.js

--- src/routes/browser.js
+++ src/routes/browser.js
@@ -1,1 +1,11 @@
+const puppeteer = require('puppeteer');
+import { Hono } from "hono";
+const index = new Hono();
 
+index.get("/send", async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://blackbox.ai');
+  const title = await page.title();
+  await browser.close();
+})();
